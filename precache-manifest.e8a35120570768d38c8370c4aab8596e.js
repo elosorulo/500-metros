1ec550d7aa697d06bc87426404af37388ab6fe78
elosorulo <manustrajman@gmail.com>
2020-09-07 05:54:13 -0300

    Updates

---
+++ precache-manifest.e8a35120570768d38c8370c4aab8596e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0d267e8ac4dce54571a5460895e62b",
+    "url": "/500-metros/index.html"
+  },
+  {
+    "revision": "57b9ca7c56495ea1a01d",
+    "url": "/500-metros/static/css/main.5ce1dfaa.chunk.css"
+  },
+  {
+    "revision": "4ffa2b65d049ea93d36f",
+    "url": "/500-metros/static/js/2.19114c25.chunk.js"
+  },
+  {
+    "revision": "c17a541c7a0f7a77dc738145c79eff2e",
+    "url": "/500-metros/static/js/2.19114c25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57b9ca7c56495ea1a01d",
+    "url": "/500-metros/static/js/main.40b4bc53.chunk.js"
+  },
+  {
+    "revision": "de2561d34d40315182c0",
+    "url": "/500-metros/static/js/runtime-main.06850b77.js"
+  },
+  {
+    "revision": "2708b82c35ac5608b8c8723919258243",
+    "url": "/500-metros/static/media/video.2708b82c.mp4"
+  }
+]);
